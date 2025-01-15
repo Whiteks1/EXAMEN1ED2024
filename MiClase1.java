fd0b05733db89f0de8b178f012db2e8cc3f6da28
Marcelo Rodas <marcelin2219@gmail.com>
2025-01-15 10:53:53 +0100

    Cambio en main

--- MiClase1.java
+++ MiClase1.java
@@ -2,7 +2,7 @@ public class MiClase1 {
     public static void main(String args[]) {
         for(int i = 0; i <= 10; i++) {
 <<<<<<< HEAD
-            System.out.println("cambio en main1");
+            System.out.println("cambio en main12");
 =======
             System.out.println("¿Cómo estas?");
 >>>>>>> f43aae655d51109060c5d2ae6fc785d346477e2a
